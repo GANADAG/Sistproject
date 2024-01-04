27c683bd0bfe9897e226042b38cb623d51798119
HYUNJU <dl06797@naver.com>
2024-01-04 16:30:18 +0900

    day0104

---
+++ java test/MyHobby.java.java
@@ -0,0 +1,8 @@
+class MyHobby{
+
+   public static void main(String [] args){
+
+
+  	 System.out.println("나의 취미는 "+args[0]+"입니다");
+	}
+}
---
+++ java test/Myinfo.java
@@ -0,0 +1,12 @@
+pubilc class Myinfo{
+	
+	public static viod main(String [] a){
+
+	/*System.out.println
+ 	System.out.println("전공"+a[0]+"입니다");
+	System.out.println("학년"+a[1]+"입니다");
+	
+	System.out.print("전공은"+a[0]+"이며 "학년은"+a[1]입니다"
+	}
+
+}
